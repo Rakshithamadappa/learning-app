2c5114ea93fe6d3dfb2df63ace7f9ea3ff443d4c
Rakshithamadappa <rakshita@targetclose.com>
2024-10-24 17:55:45 +0530

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>You clicked {count} times</Text>
+      <Button title="Click me" onPress={() => setCount(count + 1)} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
